fbb27543d27e7c2c00f090b26cfdf7bbd99a94be
brianbarth <bsbarth101@yahoo.com>
2018-10-20 12:46:30 -0500

    adding flash-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FlashCardComponent } from './flash-card/flash-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlashCardComponent
   ],
   imports: [
     BrowserModule
